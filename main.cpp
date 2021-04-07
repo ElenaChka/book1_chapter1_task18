0c70c4ea4e29af3130a7f2c2a822a10d83c79016
ElenaChka <checkalina.lena@bk.ru>
2021-04-07 23:34:44 +0300

    Aaaa

---
+++ main.cpp
@@ -0,0 +1,47 @@
+//// Программа, в которой создается массив и заполняется числами Фибоначчи
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int colichestvo;
+     long long int a, b, c;
+    a = 1;
+    b = 1;
+    cout << " Dopustimiy diapazon colichestva shisel ot 1 do 92" << endl;
+    cout << " Vvedite colichectvo chisel ";
+    cin >> colichestvo;
+    if ( colichestvo < 1 || colichestvo > 92) //and ( colichestvo > 92))
+    {
+      cout << " Vveli nedopustimoe chislo"<< endl;
+      cout << " Vvedite colichestvo chisel ";
+      cin >> colichestvo;
+    }
+    long long int massivFibonachi [ colichestvo];
+      if( colichestvo == 1)
+      {
+         massivFibonachi [0] =1;
+      }
+      if ( colichestvo == 2)
+      {
+          massivFibonachi [0] =1;
+          massivFibonachi [1] =1;
+      }
+    if ( colichestvo > 2)
+    {
+   // long long int massivFibonachi [ colichestvo];
+     for ( int n = 2; n < colichestvo; n ++)
+     {
+        massivFibonachi [0] =1;
+        massivFibonachi [1] =1;
+        c = a + b;
+        massivFibonachi [n] = c;
+        a = b;
+        b = c;
+     }
+    }
+     for ( int i =0; i < colichestvo; i++)
+     {
+       cout  << i << " element massiva = " << massivFibonachi [i] << endl;
+     }
+
+}
